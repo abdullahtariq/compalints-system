64389adf483f1cbc6511521c48318d93fa152843
abdullahtariq <faabdullahtariq@gmail.com>
2018-11-12 23:31:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 var path = require("path");
+var fs = require('fs');
 
 global.__root   = __dirname + '/';
+app.use(express.static(__dirname + '/public'));
 
 app.get('/api', function (req, res) {
   res.status(200).send('API works.');
+});
+
+app.get('/apidoc', function(req, res) {
+    fs.readFile(__dirname + '/apidoc/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
 });
 
 var UserController = require(__root + 'user/UserController');
